0c3e856883a8f06d3422e12c07f077c09e84d562
ryu9birth <129836603+ryu9birth@users.noreply.github.com>
2023-06-09 15:07:13 +0900

    6/9

--- PHP/free.php
+++ PHP/free.php
@@ -1,12 +1,27 @@
-
 <?php
-$taxrate = 0.1;
 
-$products = ["鉛筆" =>"100","消しゴム" => "150","物差し" => "500"];
-  foreach ($products as $key => $price){
-  
-    $tax_included = $price * (1 + $taxrate);
-  
-echo  $key."の税込み価格は".$tax_included."円です。<br>";
+ //商品の税込価格を計算しましょう
+  //①税率を定数TAXで作成しましょう。消費税は10%とします。
+  difine("TAX",1.1);
+
+  //②商品の情報を連想配列に入れましょう。
+
+  $products = ["鉛筆" =>"100","消しゴム" => "150","物差し" => "500"];
+
+  //③税率を計算する関数を用意します。
+  //引数には値段を受け取り、税込価格を返答します。
+  function taxincloud($price){
+    $taxincloud = $price * "TAX";
+
+return $taxincloud; 
 }
+
+  //④繰り返し文を使って画面に指定の文字を表示しましょう！
+foreach($products as $key => $price){
+
+ echo $key."の税込み価格は".$taxincloud."円です";
+
+}
+
+
 ?>
--- PHP/index.php
+++ PHP/index.php
@@ -1,16 +1,15 @@
 <?php
 
+difine("TAX","1.1");
 
 $products = ["鉛筆" =>"100","消しゴム" => "150","物差し" => "500"];
 
 
 function tax($price){
-    $taxincloud = $price + ($price * 0.1);
-}
-foreach ($products as $key => $price){
- 
-    echo $key."の税込み価格は".$taxincloud."円です";
-    echo '<br>';
+    $taxincloud = $price * 0.1;
+
+return $key."の税込み価格は".tax($price)."円です";
+    
 }
 
 
