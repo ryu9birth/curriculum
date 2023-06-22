73a595349cb2b014e60d3b9b9fe0f4b139780777
ryu9birth <129836603+ryu9birth@users.noreply.github.com>
2023-06-22 13:51:54 +0900

    6/22

--- PHP/free.php
+++ PHP/free.php
@@ -1,8 +1,8 @@
 <?php
 
- //商品の税込価格を計算しましょう
+//商品の税込価格を計算しましょう
   //①税率を定数TAXで作成しましょう。消費税は10%とします。
-  difine(TAX,1.1);
+  define(TAX,1.1);
 
   //②商品の情報を連想配列に入れましょう。
 
@@ -11,7 +11,7 @@
   //③税率を計算する関数を用意します。
   //引数には値段を受け取り、税込価格を返答します。
   function taxincloud($price){
-    $taxincloud = $price * "TAX";
+    $taxincloud = $price * TAX;
 
 return $taxincloud; 
 }
@@ -19,7 +19,7 @@ return $taxincloud;
   //④繰り返し文を使って画面に指定の文字を表示しましょう！
 foreach($products as $key => $price){
 
- echo $key."の税込み価格は".$taxincloud."円です";
+ echo $key."の税込み価格は".taxincloud($price)."円です";
 
 }
 
