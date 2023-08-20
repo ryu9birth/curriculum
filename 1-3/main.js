7ca7867e2a60211ce3ff0fc3c66e3d413f3096b7
ryu9birth <129836603+ryu9birth@users.noreply.github.com>
2023-08-20 13:57:05 +0900

    1-3

---
+++ 1-3/main.js
@@ -0,0 +1,23 @@
+//問１//
+let a = 9;
+let b = 14;
+if (a != 10){
+    console.log(a);
+}
+if (b >= 10 && b <= 20 && b % 2 === 0){
+    console.log(b);
+}
+//問２//
+let x =10;
+if (x >= 10 && x <= 20){
+    console.log('成功です');
+}else {
+    console.log('失敗です');
+}
+//問３//
+let y =0;
+if (y % 2 === 0){
+    console.log('偶数です');
+}else if (y % 2 !== 0){
+    console.log('奇数です');
+}
