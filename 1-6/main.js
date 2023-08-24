19f4d1f5e45577609fc7ada054a933c5b7b202d3
ryu9birth <129836603+ryu9birth@users.noreply.github.com>
2023-08-24 15:14:58 +0900

    1-6

---
+++ 1-6/main.js
@@ -0,0 +1,4 @@
+let scores = [10,15,20,25];
+console.log(scores);
+console.log(scores[0] + "は偶数です");
+console.log(scores[2] + "は偶数です");
---
+++ 1-6/main2.js
@@ -0,0 +1,6 @@
+let car = {
+    gass: 20.5,
+    num: 1234,
+};
+console.log("ガソリンは" + car.gass + "です");
+console.log("ナンバーは" + car.num + "です");
